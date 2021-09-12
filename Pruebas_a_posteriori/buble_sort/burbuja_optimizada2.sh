64547bcbc83df9b952c74812bbf61e9e81daa16c
angel-hache-de <angelhachede9678@gmail.com>
2021-09-12 12:35:13 -0500

    Corrección en scripts para burbujas optimizadas

--- Pruebas_a_posteriori/buble_sort/burbuja_optimizada2.sh
+++ Pruebas_a_posteriori/buble_sort/burbuja_optimizada2.sh
@@ -6,5 +6,5 @@ gcc burbuja_optimizada2.c ../tiempo.c -o bo2
 for i in  100 1000 5000 10000 50000 100000 200000 400000 500000 600000 800000 1000000 2000000 3000000 4000000 5000000 6000000 7000000 8000000 9000000 10000000
 do
     echo "Burbuja optimizada 2 con n -> $i"
-    ../bs $i < ../numeros10millones.txt >> burbuja_simple.txt
+    ../bo2 $i < ../numeros10millones.txt >> burbuja_optimizada2.txt
 done
