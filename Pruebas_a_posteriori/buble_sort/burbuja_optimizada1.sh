411cdf381d0b68e41d7b31591ec4d5d86684a208
angel-hache-de <angelhachede9678@gmail.com>
2021-09-11 20:34:19 -0500

    Ruta del archivo numeros cambiada al directorio Pruebas_a_Posteriori dentro de los scripts burbuja

--- Pruebas_a_posteriori/buble_sort/burbuja_optimizada1.sh
+++ Pruebas_a_posteriori/buble_sort/burbuja_optimizada1.sh
@@ -5,7 +5,6 @@ rm burbuja_optimizada1.txt
 gcc burbuja_optimizada1.c ../tiempo.c -o bo1
 for i in  100 1000 5000 10000 50000 100000 200000 400000 500000 600000 800000 1000000 2000000 3000000 4000000 5000000 6000000 7000000 8000000 9000000 10000000
 do
-    #../bs $i < ../numeros10millones.txt >> burbuja_simple.txt
     echo "Burbuja optimizada 1 con n -> $i"
-    ./bo1 $i < Numeros/numeros10millones.txt >> burbuja_optimizada1.txt
+    ../bs $i < ../numeros10millones.txt >> burbuja_simple.txt
 done
--- Pruebas_a_posteriori/buble_sort/burbuja_optimizada2.sh
+++ Pruebas_a_posteriori/buble_sort/burbuja_optimizada2.sh
@@ -5,7 +5,6 @@ rm burbuja_optimizada2.txt
 gcc burbuja_optimizada2.c ../tiempo.c -o bo2
 for i in  100 1000 5000 10000 50000 100000 200000 400000 500000 600000 800000 1000000 2000000 3000000 4000000 5000000 6000000 7000000 8000000 9000000 10000000
 do
-    #../bs $i < ../numeros10millones.txt >> burbuja_simple.txt
     echo "Burbuja optimizada 2 con n -> $i"
-    ./bo2 $i < Numeros/numeros10millones.txt >> burbuja_optimizada2.txt
+    ../bs $i < ../numeros10millones.txt >> burbuja_simple.txt
 done
