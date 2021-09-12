29284ed475622b6013bfa0675b348bd5b6604ec2
Marco Lg <marcolg23@gmail.com>
2021-09-11 22:53:55 -0500

    merge

---
+++ Pruebas_a_posteriori/merge_sort/merge_sort.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int i, j, k, n, p, q, r, l, C[0];
+
+void mergeSort(int* A, int p, int r);
+void merge(int* A, int p, int q, int r);
+
+int main (int argc, char* argv[]){
+    n=atoi(argv[1]);
+    int *A = (int*)malloc(n * sizeof(int));
+    for(int i = 0; i < n; i++)
+        	scanf("%d", &A[i]);
+    
+    
+
+}
+
+void mergeSort(int* A, int p, int r)
+{
+    if(p < r)
+    {
+        q = ((p+r)/2);
+        mergeSort(A, p, q);
+        mergeSort(A, q+1, r);
+        merge(A, p, q, r);
+    }
+}
+
+
+void merge(int* A, int p, int q, int r)
+{
+    l=r-p+1;
+    i=p;
+    j=q+1;
+    for (size_t k = 0; k >=1; k++)
+    {
+        if (i<=q && j<=r)
+        {
+            if (A[i]<A[j])
+            {
+                C[k]=A[i];
+                i++;
+            }
+        else if (i<=q)
+        {
+            C[k]=A[i];
+        i++;
+        }
+        else
+        {
+            C[k]=A[j];
+            j++;
+        }
+        }
+    A[p-r] = C[k];
+    }
+}
---
+++ Pruebas_a_posteriori/quick_sort/quick_sort.c
@@ -0,0 +1,47 @@
+
+
+void quickSort(A, p, r);
+void pivote(A, p ,r);
+void intercambiar(A, i, j);
+
+
+
+void quickSort(A, p, r)
+{
+    if (p<r)
+    {
+        j=pivote(A, p, r);
+        quickSort(A, p, j-1);
+        quickSort(A, j+1, r);
+    }
+}
+
+void pivote(A, p ,r)
+{
+    piv=A[p];
+    i=p+1;
+    j=r;
+    while (A[i]<piv && i<r)
+    {
+        i++;
+        while (A[j]>piv)
+        {
+            j--;
+        }
+        if (i<=j)
+        {
+            intercambiar(A, i, j);
+        }
+        else
+        {
+            intercambiar(A, p, j);
+        }
+    }
+}
+
+void intercambiar(A, i, j)
+{
+    temp=A[j];
+    A[j]=A[i];
+    A[i]=temp;
+}
