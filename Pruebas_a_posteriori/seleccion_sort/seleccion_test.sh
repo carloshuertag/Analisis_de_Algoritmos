9a1ac2f73603042265e18e320afeae6b64e8c946
ReinaLeonel <82613093+ReinaLeonel@users.noreply.github.com>
2021-09-12 00:38:19 -0500

    script de seleccion

---
+++ Pruebas_a_posteriori/seleccion_sort/seleccion_test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# sudo chmod 755 seleccion_test.sh
+
+gcc seleccion_time.c ../tiempo.c -o seleccion
+for i in  100 1000 5000 10000 50000 100000 200000 400000 500000 600000 800000 1000000 2000000 3000000 4000000 5000000 6000000 7000000 8000000 9000000 10000000
+do
+    echo "Insercion con n -> $i"
+    echo .::tiempo con n = $i >> insercion_test.txt
+    ./seleccion $i < ../numeros10millones.txt >> insercion_test.txt
+done
