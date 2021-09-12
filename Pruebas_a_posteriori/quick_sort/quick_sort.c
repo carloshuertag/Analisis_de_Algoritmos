a9ed544c0274896f8aa030423a36536e4e04e1a2
Marco Lg <marcolg23@gmail.com>
2021-09-12 13:59:10 -0500

    quick

--- Pruebas_a_posteriori/quick_sort/quick_sort.c
+++ Pruebas_a_posteriori/quick_sort/quick_sort.c
@@ -1,72 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int i, j, n, p, r;
+int i, j, n;
 
-void quickSort(int* A, int p, int r);
+void quickSort(int* arr, int l, int h);
 int pivote(int* A, int p, int r);
 void intercambiar(int* A, int i, int j);
 
 int main (int argc, char* argv[]){
     n=atoi(argv[1]);
-    int *A = (int*)malloc(n * sizeof(int));
-    int *C = (int*)malloc(n * sizeof(int));
+    int *arr = (int*)malloc(n * sizeof(int));
     for(int i = 0; i < n; i++)
-        	scanf("%d", &A[i]);
+        	scanf("%d", &arr[i]);
     
     printf("Sin ordenar:");
     for(int i = 0; i < n; i++)
         {
-            printf("%d ", A[i]);
+            printf("%d ", arr[i]);
         }
 
-    quickSort(A, 0, n-1);
+    quickSort(arr, 0, n-1);
 
     printf("\nOrdenados:");
     for(int i = 0; i < n; i++)
         {
-            printf("%d ", A[i]);
+            printf("%d ", arr[i]);
         }
 }
 
-void quickSort(int* A, int p, int r)
+void quickSort(int* arr, int l, int h)
 {
-    if (p<r)
+    if (l<h)
     {
-        j=pivote(A, p, r);
-        quickSort(A, p, j-1);
-        quickSort(A, j+1, r);
+        int pi=pivote(arr, l, h);
+        quickSort(arr, l, pi-1);
+        quickSort(arr, pi+1, h);
     }
 }
 
-int pivote(int* A, int p, int r)
+int pivote(int* arr, int l, int h)
 {
-    int piv=A[p];
-    i=p+1;
-    j=r;
-    while (A[i]<piv && i<r)
+    int piv=arr[h];
+    i=l-1;
+    j=l;
+
+    //
+    for (j = l; j <= h-1; j++)
     {
-        i++;
-        while (A[j]>piv)
+        if (arr[j] < piv)
         {
-            j--;
-        }
-        if (i<j)
-        {
-            intercambiar(A, i, j);
-        }
-        else
-        {
-            intercambiar(A, p, j);
-            
+            i++;
+            intercambiar(arr, i, j);
         }
     }
-    return (j);
+    intercambiar(arr, i+1, h);
+    return (i + 1);
+    //
 }
 
-void intercambiar(int* A, int i, int j)
+void intercambiar(int* arr, int i, int j)
 {
-    int temp=A[j];
-    A[j]=A[i];
-    A[i]=temp;
+    int temp=arr[j];
+    arr[j]=arr[i];
+    arr[i]=temp;
 }
