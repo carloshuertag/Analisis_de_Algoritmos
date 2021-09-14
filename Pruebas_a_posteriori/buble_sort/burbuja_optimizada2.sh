549e6fa7dbc2a7172b8245975ae3a8cb04871240
Carlos Huerta Garcia <huerta2502@gmail.com>
2021-09-14 17:42:41 -0500

    last tests

--- Pruebas_a_posteriori/buble_sort/burbuja_optimizada2.sh
+++ Pruebas_a_posteriori/buble_sort/burbuja_optimizada2.sh
@@ -3,7 +3,7 @@
 #Borra el archivo anterior de mediciones
 rm burbuja_optimizada2.txt
 gcc burbuja_optimizada2.c ../tiempo.c -o bo2
-for i in  100 1000 5000 10000 50000 100000 200000 400000 500000 600000 800000 1000000 2000000 3000000 4000000 5000000 6000000 7000000 8000000 9000000 10000000
+for i in  20 40 60 80 100 200 400 600 800 1000 2000 4000 6000 8000 10000 20000 40000 60000 80000 100000
 do
     echo "Burbuja optimizada 2 con n -> $i"
     ./bo2 $i < ../../../numeros10millones.txt >> burbuja_optimizada2.txt
