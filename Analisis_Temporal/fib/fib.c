386b920877dde3cc535ee03cd1177b7f6d208ea3
marcolg23 <marcolg23@gmail.com>
2021-09-25 09:18:26 -0500

    fib sin tiempo

--- Analisis_Temporal/fib/fib.c
+++ Analisis_Temporal/fib/fib.c
@@ -1,123 +1,84 @@
 /**
- *  Programa de prueba para la búsqueda por árbol (iterativa)
- *  @author @huerta2502 Carlos Huerta García
- *  @copyright Septiembre 2021
+ *  @author @Reina Beatriz Juarez Leonel
+ *  Curso: Análisis de algoritmos
+ *  (C) Septiembre 2021
  *  @version 1.0
- *  Compilación: gcc bst_test.c ../tiempo.c -o bst_test
- *  Ejecución: ./bst_test n < ../../../numeros10millones.txt
+ *  ESCOM-IPN
+ *  Busqueda de un numero x dentro de un arreglo ordenado de tamaño n, ocupando la busqueda exponencial en C.
+ *  Compilación: "gcc exponencialBusqueda.c -o exponencial"
+ *  Ejecución: "exponencial n x <../../../10millones.txt"
 */
-
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int i;
+int n;                          // TMAÑO DE LA SECUENCIA
+int *arrfib;                    // ARREGLO PARA CADA VALOR
+int i;                          
 
-int fib(int n);
-
-int fib(int n)
-{
-    if (n <= 1)
-        return n;
-    return fib(n-1)+fib(n-2);
-}
+void *runn(void *arg);
 
 int main(int argc, char *argv[])
 {
-    int n, item;
-    if (argc != 3)
+    if (argc != 2)
     {
-        fprintf(stderr, "\nPlease enter the array length, e.g.: %s 10000 123456\n",
-                argv[0]);
-        exit(1);
+        printf("el formato es:./fib <valor>\n");
+        return -1; //ERROR
     }
+
+    if (atoi(argv[1]) < 0)
+    {
+        printf("%d debe ser>0\n", atoi(argv[1]));
+        return -1; //ERROR
+    }
+
+    n = atoi(argv[1]);
+    arrfib = (int *)malloc(n * sizeof(int)); //TAMAÑO DEL ARREGLO
+    pthread_t *threads = (pthread_t *) malloc(n * sizeof(pthread_t)); //TAMAÑO DEL ARREGLO DE HILOS
+    pthread_attr_t attr; //ATRIBUTO DEL HILO
+
+    pthread_attr_init(&attr);
+
+    for (i=0; i<n; i++)
+    {
+        pthread_create(&threads[i], &attr, runn, NULL);
+    }                           //CREACIÓN DE HILOS
+
+    int j;
+
+    for (j = 0; j < n; j++)
+    {
+        pthread_join(threads[j], NULL);
+    }                           // End of wating the threads to exit.
+
+    // printing arrfib.
+    printf("The Fibonacci sequence.:");
+    int k;
+
+    for (k = 0; k < n; k++)
+    {
+        printf("%d,", arrfib[k]);
+    }                           // End of printing arrfib.
+    return 0;
+}                               // End of main.
+
+void *runn(void *arg)
+{
+    if (i == 0)
+    {
+        arrfib[i] = 0;
+        pthread_exit(0);
+    }                           // first fib term
+
+    if (i == 1)
+    {
+        arrfib[i] = 1;
+        pthread_exit(0);
+    }                           // seconed fib term
     else
     {
-        n = atoi(argv[1]); // Toma del argumento la longitud del arreglo
-        item = atoi(argv[2]); // item to search on array
-    }
-    int *arr = (int *)malloc(n * sizeof(int)); // asigna la memoria para el arreglo
-    if (arr == NULL)
-    {
-        perror("Memory not allocated.\n");
-        exit(1);
-    }
-    for (i = 0; i < n; i++)
-        scanf("%d", &arr[i]); // guarda los elemetos introducidos en el arreglo
-
-}
-
-
-#include <stdio.h>
- 
-// Utility function to find minimum of two elements
-int min(int x, int y) { return (x <= y) ? x : y; }
- 
-/* Returns index of x if present,  else returns -1 */
-int fibMonaccianSearch(int arr[], int x, int n)
-{
-    /* Initialize fibonacci numbers */
-    int fibMMm2 = 0; // (m-2)'th Fibonacci No.
-    int fibMMm1 = 1; // (m-1)'th Fibonacci No.
-    int fibM = fibMMm2 + fibMMm1; // m'th Fibonacci
- 
-    /* fibM is going to store the smallest Fibonacci
-       Number greater than or equal to n */
-    while (fibM < n) {
-        fibMMm2 = fibMMm1;
-        fibMMm1 = fibM;
-        fibM = fibMMm2 + fibMMm1;
-    }
- 
-    // Marks the eliminated range from front
-    int offset = -1;
- 
-    /* while there are elements to be inspected. Note that
-       we compare arr[fibMm2] with x. When fibM becomes 1,
-       fibMm2 becomes 0 */
-    while (fibM > 1) {
-        // Check if fibMm2 is a valid location
-        int i = min(offset + fibMMm2, n - 1);
- 
-        /* If x is greater than the value at index fibMm2,
-           cut the subarray array from offset to i */
-        if (arr[i] < x) {
-            fibM = fibMMm1;
-            fibMMm1 = fibMMm2;
-            fibMMm2 = fibM - fibMMm1;
-            offset = i;
-        }
- 
-        /* If x is greater than the value at index fibMm2,
-           cut the subarray after i+1  */
-        else if (arr[i] > x) {
-            fibM = fibMMm2;
-            fibMMm1 = fibMMm1 - fibMMm2;
-            fibMMm2 = fibM - fibMMm1;
-        }
- 
-        /* element found. return index */
-        else
-            return i;
-    }
- 
-    /* comparing the last element with x */
-    if (fibMMm1 && arr[offset + 1] == x)
-        return offset + 1;
- 
-    /*element not found. return -1 */
-    return -1;
-}
- 
-/* driver function */
-int main(void)
-{
-    int arr[]
-        = { 10, 22, 35, 40, 45, 50, 80, 82, 85, 90, 100,235};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int x = 235;
-      int ind = fibMonaccianSearch(arr, x, n);
-  if(ind>=0)
-    printf("Found at index: %d",ind);
-  else
-    printf("%d isn't present in the array",x);
-    return 0;
-}
+        arrfib[i] = arrfib[i - 1] + arrfib[i - 2];
+        // printf("arrfib[%d]%d,\n",i,arrfib[i]);
+        pthread_exit(0);        // thread exit.
+    }                           // End of else
+}                               // End of run.
