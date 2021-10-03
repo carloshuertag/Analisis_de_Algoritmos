ee36e4cddd3f2ec19e72071960507b9c787d8ce1
Carlos Huerta Garcia <huerta2502@gmail.com>
2021-10-03 17:27:54 -0500

    exp search with threads tested

--- Analisis_Temporal/exponencial/exponencialHilos.sh
+++ Analisis_Temporal/exponencial/exponencialHilos.sh
@@ -7,5 +7,5 @@ gcc expHilosFix_time.c ../tiempo.c -o exponencialHilos_time -lpthread
 for n in  1000000 2000000 3000000 4000000 5000000 6000000 7000000 8000000 9000000 10000000
 do
         echo "Busqueda exponencial_hilos con n = $n"
-        ./exponencialHilos_time  $n < ../../../10millones.txt >> exponencialHilos_time.txt
+        ./exponencialHilos_time $n < ../../../10millones.txt >> exponencialHilos_time.txt
 done
