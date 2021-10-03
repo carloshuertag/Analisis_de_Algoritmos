fd431dc46d03f2d158d53a0124b1ff7e3202795a
Carlos Huerta Garcia <huerta2502@gmail.com>
2021-10-03 15:12:04 -0500

    thread testing started

--- Analisis_Temporal/b_binaria/binaria_hilos.sh
+++ Analisis_Temporal/b_binaria/binaria_hilos.sh
@@ -5,5 +5,5 @@ gcc b_binaria_hilos.c ../tiempo.c -o bbh -lpthread
 for i in  1000000 2000000 3000000 4000000 5000000 6000000 7000000 8000000 9000000 10000000
 do
     echo "Binaria_hilos con n = $i"
-    ./bbh $i < numeros10millones.txt >> binaria_hilos.txt
+    ./bbh $i < ../../../10millones.txt >> binaria_hilos.txt
 done
