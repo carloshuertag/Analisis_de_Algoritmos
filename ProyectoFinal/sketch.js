1d3a3f00123e1a0d245db8cb5288ee502db7bc66
Marco Lavarrios <marcolg23@gmail.com>
2021-11-17 14:05:53 -0600

    funcionando p5js

---
+++ ProyectoFinal/sketch.js
@@ -0,0 +1,12 @@
+function setup(){
+    createCanvas(800,600)
+}
+
+function draw(){
+    if (mouseIsPressed) {
+        fill(0);
+        ellipse(mouseX, mouseY, 20, 20);
+      } else {
+      }
+      
+}
