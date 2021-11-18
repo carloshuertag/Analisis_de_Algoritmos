dc73141a8c785922a17edd11acdd09e5e103b10d
Marco Lavarrios <marcolg23@gmail.com>
2021-11-17 22:55:20 -0600

    points

--- ProyectoFinal/sketch.js
+++ ProyectoFinal/sketch.js
@@ -1,12 +1,27 @@
+class Point {
+    constructor(x, y, b){
+    this.x = x;
+    this.y = y;
+    this.b = b;
+    }
+}
+
+let dots = [];
+let count = 0;
 function setup(){
-    createCanvas(800,600)
+    //COLORES
+    //
+    createCanvas(windowWidth,windowHeight);
+    background(150);
 }
 
 function draw(){
-    if (mouseIsPressed) {
-        fill(0);
-        ellipse(mouseX, mouseY, 20, 20);
-      } else {
-      }
-      
 }
+
+function mouseClicked() {
+    ellipse(mouseX, mouseY, 10, 10);
+        let dot = new Point(mouseX, mouseY, 0);
+        dots.push(dot);
+        console.log(dots);
+        count++;
+  }
